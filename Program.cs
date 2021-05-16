534b0ae16d662f4bd0296b2c35416f317308cc24
user <exsample@mail.com>
2021-05-16 18:59:36 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using CommandLine;
+using System;
+using System.IO;
+
+namespace NMapLogParser
+{
+    class Program
+    {
+        public class Options
+        {
+            [Option('f', "file", Required = true, HelpText = "Set file to parse")]
+            public string File { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            string filePath = "";
+
+            var parseResult = Parser.Default.ParseArguments<Options>(args)
+                .WithParsed(o =>
+                {
+                    filePath = o.File;
+                });
+
+            if (parseResult.Tag == ParserResultType.NotParsed)
+            {
+                Environment.Exit(0);
+            }
+
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine(@$"File {filePath} not found");
+                Environment.Exit(0);
+            }
+
+            string readText = File.ReadAllText(filePath);
+            Console.WriteLine(readText);
+        }
+    }
+}
