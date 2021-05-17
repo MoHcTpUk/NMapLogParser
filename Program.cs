f3d98fe70e56ee69a04958ca1533449d4c5994d5
user <exsample@mail.com>
2021-05-17 14:51:19 +0300

    Сделан функционал разбивки отчета по IP адресам

--- Program.cs
+++ Program.cs
@@ -2,6 +2,9 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace NMapLogParser
 {
@@ -11,6 +14,7 @@ namespace NMapLogParser
         {
             [Option('f', "file", Required = true, HelpText = "Set file to parse")]
             public string File { get; set; }
+
         }
 
         static void Main(string[] args)
@@ -55,6 +59,50 @@ namespace NMapLogParser
                 reports.RemoveAt(0);
 
             Console.WriteLine($@"Finded {reports.Count} reports");
+
+            foreach (var report in reports)
+            {
+                Regex regexIpAdd = new Regex(@"[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}");
+                MatchCollection ipAddMatches = regexIpAdd.Matches(report);
+                if (ipAddMatches.Count > 0)
+                {
+                    var directoryReport = Path.GetDirectoryName(filePath);
+                    var reportFileName = $@"{ipAddMatches.First().Value.Replace('.','-')}.txt";
+
+                    if (string.IsNullOrEmpty(directoryReport) || string.IsNullOrEmpty(reportFileName))
+                    {
+                        Console.WriteLine(@$"Error while processing {reportFileName} report: directory or file not found");
+                        continue;
+                    }
+                    
+                    var reportFilePath = Path.Combine(directoryReport, reportFileName);
+
+                    Console.Write(@$"Writing {reportFileName} report... ");
+
+                    if (File.Exists(reportFilePath))
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine(@$"File {reportFilePath} already exists. Rewrite? Y/N");
+                        var answer = Console.ReadKey();
+                        Console.WriteLine();
+                        if (answer.Key != ConsoleKey.Y)
+                        {
+                            Console.WriteLine(@$"File {reportFilePath} skiped!");
+                            continue;
+                        }
+                    }
+
+                    using FileStream fs = File.Create(reportFilePath);
+                    byte[] info = new UTF8Encoding(true).GetBytes(report);
+                    fs.Write(info, 0, info.Length);
+
+                    Console.WriteLine(@$"Done!");
+                }
+                else
+                {
+                    Console.WriteLine("Not finded report IP");
+                }
+            }
         }
     }
 }
